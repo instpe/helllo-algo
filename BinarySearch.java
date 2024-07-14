6cc5522d5b274acca8f139c13349db813c02b882
wei <493703217@qq.com>
2024-07-14 16:34:20 +0800

    feat: solution of searchandinsert
    Signed-off-by: wei <493703217@qq.com>

--- BinarySearch.java
+++ BinarySearch.java
@@ -15,4 +15,21 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    // solution of https://leetcode.cn/problems/search-insert-position/
+    public int searchAndInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int middle = left + (right - left)/2;
+            if (nums[middle] > target) {
+                right = middle - 1;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return left;
+    }
 }
