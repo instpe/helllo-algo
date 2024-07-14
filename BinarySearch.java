d1190fd1cbc3112b2aa6e5caaac74b8e519f2399
wei <493703217@qq.com>
2024-07-14 14:41:38 +0800

    feat: solution of binarysearch
    Signed-off-by: wei <493703217@qq.com>

---
+++ BinarySearch.java
@@ -0,0 +1,18 @@
+public class BinarySearch {
+    // solution of https://leetcode.cn/problems/binary-search/
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int middle = left + (right - left)/2;
+            if (nums[middle] > target) {
+                right = middle - 1;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+}
