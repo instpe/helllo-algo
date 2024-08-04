032d38730b2462126a4e8bca36d7eb9c75a41094
wei <493703217@qq.com>
2024-08-04 18:26:49 +0800

    feat: solution of sqrtx
    Signed-off-by: wei <493703217@qq.com>

--- BinarySearch.java
+++ BinarySearch.java
@@ -32,4 +32,81 @@ public class BinarySearch {
         }
         return left;
     }
+
+
+    // solution of  https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/solutions/
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = findFirstPosition(nums, target);
+        result[1] = findLastPosition(nums, target);
+        return result;
+    }
+
+    private int findFirstPosition(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                if (mid == 0 || nums[mid - 1] != target) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private int findLastPosition(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                if (mid == nums.length - 1 || nums[mid + 1] != target) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    // solution of https://leetcode.cn/problems/sqrtx/description/
+    public int mySqrt(int x) {
+        if (x < 2) {
+            return x;
+        }
+
+        int low = 1, high = x / 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long square = (long) mid * mid;
+
+            if (square == x) {
+                return mid;
+            } else if (square < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        // Specifically:
+        // When low exceeds high, high has already been the last mid value that satisfies mid * mid <= x.
+        // low, on the other hand, is the first mid value that makes mid * mid > x.
+        // Therefore, returning high ensures that the result is the closest integer part of the square root of x that does not exceed x.
+        return high; // why high?
+
+    }
 }
